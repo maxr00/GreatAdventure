8a5cb57102f7e1bb3ffbc0f426b53fd297b5d3d8
Max <max@rauffer.com>
2019-11-05 22:22:56 -0800

    windows for the Buildingizer

--- GreatAdventure/Assets/Scripts/Buildingizer.cs
+++ GreatAdventure/Assets/Scripts/Buildingizer.cs
@@ -7,11 +7,14 @@ public class Buildingizer : MonoBehaviour
 {
     public GameObject cornerBricks;
     public float repeatsPerScale = 1;
-    public float bricksInset = 0.5f;
+    public float bricksInset = 0.3f;
     public float brickYOff = 0.5f;
 
     public GameObject window;
     public float windowsPerScale = 0.2f;
+
+    public float floor1WindowsY = 1.5f;
+    public float floorWindowDistance = 6;
 
     public void Buildingize()
     {
@@ -43,10 +46,31 @@ public class Buildingizer : MonoBehaviour
         }
 
         // Windows
-        Vector3 numWindows = new Vector3Int((int)(windowsPerScale * scale.x), 0, (int)(windowsPerScale * scale.z));
-        for(int x = 0; x < numWindows.x; x++)
-        {
+        float boundsOverF1 = Mathf.Max(transform.position.y + maxBound.y - floor1WindowsY, 0);
 
+        Debug.Log(boundsOverF1 / floorWindowDistance);
+
+        Vector3 numWindows = new Vector3Int(
+            (int)(windowsPerScale * scale.x), 
+            (int)Mathf.Max( Mathf.Round(boundsOverF1 / floorWindowDistance), 1), 
+            (int)(windowsPerScale * scale.z));
+        Vector3 windowDist = new Vector3(scale.x / numWindows.x, 0, scale.z / numWindows.z);
+        Vector3 start = new Vector3(-maxBound.x + windowDist.x / 2, 0, -maxBound.z + windowDist.z / 2);
+
+        for(int y = 0; y < numWindows.y; y++)
+        {
+            float yp = floor1WindowsY + y * floorWindowDistance - transform.position.y;
+            for(int x = 0; x < numWindows.x; x++)
+            {
+                GameObject.Instantiate(window, transform.position + new Vector3(start.x + x * windowDist.x, yp, maxBound.z), Quaternion.Euler(0, 180, 0), parent.transform);
+                GameObject.Instantiate(window, transform.position + new Vector3(start.x + x * windowDist.x, yp, -maxBound.z), Quaternion.Euler(0, 0, 0), parent.transform);
+            }
+            for (int z = 0; z < numWindows.z; z++)
+            {
+                GameObject.Instantiate(window, transform.position + new Vector3(maxBound.x, yp, start.z + z * windowDist.z), Quaternion.Euler(0, 270, 0), parent.transform);
+                GameObject.Instantiate(window, transform.position + new Vector3(-maxBound.x, yp, start.z + z * windowDist.z), Quaternion.Euler(0, 90, 0), parent.transform);
+            }
         }
+
     }
 }
